10ec77f379ea71f510c00be2fab28e05afc1e48e
Italo Gonçalves <101333657+Italogoncalves1@users.noreply.github.com>
2022-07-29 10:34:48 -0300

    a

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import {FaStar} from "react-icons/fa"
+
+const imageUrl = import.meta.env.VITE_IMG
+
+const MovieCard = ({movie}) => {
+  return (
+    <div>MovieCard</div>
+  )
+}
+
+export default MovieCard
